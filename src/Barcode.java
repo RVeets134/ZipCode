1a1bd7b34e618690c533660464af3a2ce342d93f
vchowdhary <vanshikachowdhary@gmail.com>
2016-10-24 17:28:44 -0400

    Add files via upload

--- src/Barcode.java
+++ src/Barcode.java
@@ -33,18 +33,52 @@ public class Barcode {
 	}
 
 	public Zipcode toZipcode() {
-		// TODO Auto-generated method stub
-		return null;
+		String zip = "";
+		for(int i = 1; i<postableBarcode.length()-6; i+=5)
+		{
+			String digitCode = postableBarcode.substring(i,  i+5);
+			zip+=getDigit(digitCode);
+		}
+		Zipcode zipcode = new Zipcode(Integer.parseInt(zip));
+		return zipcode;
 	}
 
 	public int getCheckDigit() {
-		// TODO Auto-generated method stub
-		return 0;
+		String checkDigitString = postableBarcode.substring(postableBarcode.length() - 6, postableBarcode.length()-1);
+		int checkDigit = Integer.parseInt(getDigit(checkDigitString));
+		return checkDigit;
 	}
 
 	public boolean checkValidity(Zipcode convertedZipcode, int checkDigit) {
-		// TODO Auto-generated method stub
+		int sumOfDigits = getSumOfDigits(convertedZipcode.getZipcode());
+		if((sumOfDigits + checkDigit)%10 == 0) return true;
 		return false;
+	}
+	
+	private int getSumOfDigits(int num)
+	{
+		int sum = 0;
+		while(num>0)
+		{
+			sum+=num%10;
+			num = num/10;
+		}
+		return sum;
+	}
+	private String getDigit(String digitCode)
+	{
+		int digit[] = new int[5];
+		int digitNum = 0;
+		for(int i = 0; i<digitCode.length(); i++)
+		{
+			if(digitCode.substring(i,i+1).equals("|"))
+				digit[i] = 1;
+			else
+				digit[i] = 0;
+		}
+		digitNum = digit[0]*7 + digit[1]*4 + digit[2]*2 + digit[3];
+		if(digitNum == 11) digitNum = 0;
+		return digitNum+"";
 	}
 
 }
