3f8679a976736062ab2bb7b6d17ac53801d1db8a
RVeets134 <rakesh.veetekat@gmail.com>
2016-11-02 13:56:54 -0400

    Finished adding comments

--- src/Location.java
+++ src/Location.java
@@ -1,17 +1,25 @@
-
+/*
+ * Rakesh Veetekat
+ * 11/1/16
+ * Period 6 Computer Science
+ * Location class
+ */
 public class Location {
 
+	//Instance data
 	private String city;
 	private String state;
 	
 	public Location(String city,String state)
 	{
 	
+		//Set the city and state instance data to the parameters given
 		this.city = city;
 		this.state = state;
 		
 	}
 	
+	//Return the city of the location object
 	public String getCity()
 	{
 		
@@ -19,6 +27,7 @@ public class Location {
 		
 	}
 	
+	//Return the state of the location object
 	public String getState()
 	{
 		
@@ -26,6 +35,7 @@ public class Location {
 		
 	}
 	
+	//Return the city and state seperated by a space as a string
 	public String toString()
 	{
 		
--- src/Zipcode.java
+++ src/Zipcode.java
@@ -1,9 +1,17 @@
+/*
+ * Rakesh Veetekat
+ * 11/1/16
+ * Period 6 Computer Science
+ * Zipcode class
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Zipcode{
 
+	//Instance data
+	
 	private int zipcode;
 	private int zipcodeWithCheck;
 	private String zipcodeString;
@@ -15,14 +23,26 @@ public class Zipcode{
 	public Zipcode(int zipcode)
 	{
 	
+		//Store the zipcode for the object
+		
 		this.zipcode = zipcode;
 		zipcodeString = Integer.toString(zipcode);
-		if(zipcodeString.length() == 4) zipcodeString = "0" + zipcodeString;
+		
+		//If zipcode has 4 digits, add 0 to beginning
+		
+		if(zipcodeString.length() == 4)
+		{
+			zipcodeString = "0" + zipcodeString;
+		}
+		
+		//Create zipcode with check digit at end
+		
 		createZipWithCheck();
 		zipcodeStringWithCheck = Integer.toString(zipcodeWithCheck);
 		
 	}
 	
+	//Returns the zipcode of the zipcode object
 	public int getZipcode()
 	{
 		
@@ -30,6 +50,7 @@ public class Zipcode{
 		
 	}
 	
+	//Finds the check digit for the zipcode and adds it to the end of the zipcode to create a new variable
 	private void createZipWithCheck()
 	{
 		
@@ -39,17 +60,21 @@ public class Zipcode{
 		for(int n = 0;n < zipcodeString.length();n++)
 		{
 			
+			//Get the sum of the digits of the zipcode
 			sum += zipcodeString.charAt(n);
 			
 		}
 		
+		//The check digit is found by adding enough to the sum of the zipcode digits to make the sum a multiple of 10
 		checkDigit = 10 - (sum % 10);
 		
+		//Raise the zipcode by a multiple of 10 and add check digit to the end
 		zipcodeWithCheck = (zipcode * 10) + checkDigit;
 		
 		
 	}
 	
+	//Read through the file to find the amount of locations the zipcode refers to
 	private void locationArraySize() throws FileNotFoundException
 	{
 		File zip = new File("ZipCodesCity.txt");
@@ -57,61 +82,87 @@ public class Zipcode{
 		String line = cities.nextLine();
 		int numberOfLocations = 0;
 		
+		//Loop to read through all the cities
 		while(cities.hasNext())
 		{
+			//Checks if the current location has the same zipcode as the zipcode object
 			if(line.contains(zipcodeString))
 			{
 
+				//Updates the number of locations the zipcode refers to each time it finds one
+				
 				numberOfLocations++;
 			
 			}
+			
+			//Move onto the next line of locations after checking the previous line
 			line = cities.nextLine();
+			
 		}
 
+		//Update the instance data for the zipcode object
 		this.numberOfLocations = numberOfLocations;
 		
 	}
 	
+	//Creates the location array containing all the locations that can be found using the zipcode
 	private void createLocationArray() throws FileNotFoundException
 	{
 		
+		//Sets the location array size instance data for the zipcode object
+	
 		locationArraySize();
+		
 		Location[] locations = new Location[numberOfLocations];
 		File zip = new File("ZipCodesCity.txt");
+		
+		//Reset the scanner for the cities text file
 		Scanner cities = new Scanner(zip);
 		String line = cities.nextLine();
 		String city;
 		String state;
+		
+		//Keeps track of which index of the location array has to be filled
 		int arrayCounter = 0;
 		
+		//Loop to read through the cities
 		while(cities.hasNext())
 		{
 
+			//Checks to find the locations with the zipcode
 			if(line.contains(zipcodeString))
 			{
 
+				//Skips the zipcode portion of the line
 				line = line.substring(5);
+				
 				int n = line.indexOf(",");
 				line = line.substring(n+1);
 				n = line.indexOf(",");
 		
+				//Set the values for the city and state strings for each location
 				city = line.substring(0,n);
 				state = line.substring(n+1);
 				
+				//Create each location using the city and state strings
 				locations[arrayCounter] = new Location(city,state);
 		
+				//Increase the index counter for the array each time one is filled with a location
 				arrayCounter++;
 			
 			}
 			
+			//Move to next line after checking the last one
 			line = cities.nextLine();
 			
 		}
 		
+		//Set the instance data for all the locations in the object to the locations array created in this method
 		this.locations = locations;
 		
 	}
 	
+	//Returns an array of all the location objects for a zipcode object
 	public Location[] getLocationArray() throws FileNotFoundException
 	{
 		
@@ -120,6 +171,7 @@ public class Zipcode{
 		
 	}
 	
+	//Returns all the locations of the zipcode object as a String
 	public String toString()
 	{
 		
@@ -135,6 +187,7 @@ public class Zipcode{
 	
 	}
 	
+	//This method creates the barcode for the zipcode, including the bar for the check digit
 	private void createBarcode() 
 	{
 		
@@ -157,12 +210,16 @@ public class Zipcode{
 		for(int i = 0; i < zipcodeString.length(); i++)
 		   {
 			
+			//Check each digit of zipcode and convert to respective bar
+			
 		    barcode += converter[Integer.parseInt(zipcodeStringWithCheck.substring(i,i+1))];
 		   
 		   }
-		barcode+="|";
+		
+		barcode += "|";
 	}
 	
+	//This method returns the barcode of the zipcode with the check digit
 	public String getBarcode()
 	{
 		
@@ -171,9 +228,11 @@ public class Zipcode{
 		
 	}
 
+	//This method returns the zipcode of the object as a string
 	public String getZipcodeString() {
-		// TODO Auto-generated method stub
+		
 		return zipcodeString;
+		
 	}
 
 }
